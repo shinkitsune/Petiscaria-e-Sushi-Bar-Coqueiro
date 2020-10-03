97b252976ee775ec63692322bb416f31c42d1300
Elton <eltonandradee@gmail.com>
2020-10-03 16:44:03 -0300

    Projeto da landing page Petiscaria e Sushi Bar Macaubal

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Petiscaria-e-Sushi-Bar-Coqueiro
+
+Projeto de landing page para a Petiscaria e Sushi Bar Macaubal.~~~~
---
+++ js/script.js
@@ -0,0 +1,34 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 5
+    }
+  }
+})
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { myFunction() };
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
